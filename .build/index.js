56f4313917d1dea2f324bae9cb512ad34920a006
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-25 15:17:34 +0000

    e

--- .build/index.js
+++ .build/index.js
@@ -1,4 +1,13 @@
 const x = ["a", "b", "c"];
 const y = [1, 2, 3];
 console.log("x:", x, "y:", y);
+const stringData = {
+  data: "mad",
+  message: true
+};
+const stringErrorData = {
+  data: 1,
+  message: false
+};
+console.log("ok:", stringData, "fail:", stringErrorData);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -7,3 +7,20 @@ const x: X = ["a", "b", "c"]
 const y: Y = [1, 2, 3]
 
 console.log("x:", x, "y:", y)
+
+interface StringDataType<T extends boolean> {
+  readonly data: T extends true ? string : number
+  readonly message: T
+}
+
+const stringData: StringDataType<true> = {
+  data: "mad",
+  message: true
+}
+
+const stringErrorData: StringDataType<false> = {
+  data: 1,
+  message: false
+}
+
+console.log("ok:", stringData, "fail:", stringErrorData)
