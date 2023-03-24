756a8c42d411e01c914058b1829e3960ac333da0
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-03-24 02:57:05 +0000

    e

--- .build/index.js
+++ .build/index.js
@@ -1,13 +1,3 @@
-const x = ["a", "b", "c"];
-const y = [1, 2, 3];
-console.log("x:", x, "y:", y);
-const stringData = {
-  data: "mad",
-  message: true
-};
-const stringErrorData = {
-  data: 1,
-  message: false
-};
-console.log("ok:", stringData, "fail:", stringErrorData);
+const x = "mad";
+console.log(x);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,26 +1,3 @@
-type MyReturnType<T> = T extends string ? string[] : number[]
-
-type X = MyReturnType<string> // []string
-type Y = MyReturnType<number> // []int
-
-const x: X = ["a", "b", "c"]
-const y: Y = [1, 2, 3]
-
-console.log("x:", x, "y:", y)
-
-interface StringDataType<T extends boolean> {
-  readonly data: T extends true ? string : number
-  readonly message: T
+type CustomPickType<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? P : never]: T[P]
 }
-
-const stringData: StringDataType<true> = {
-  data: "mad",
-  message: true
-}
-
-const stringErrorData: StringDataType<false> = {
-  data: 1,
-  message: false
-}
-
-console.log("ok:", stringData, "fail:", stringErrorData)
