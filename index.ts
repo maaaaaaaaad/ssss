779827a41f3337f82895ee753fe8dd7c7a2f9896
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-03-24 09:01:37 +0000

    e

--- index.ts
+++ index.ts
@@ -1,2 +1,22 @@
-type CustomReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : T
-type CustomPickType<T, K extends keyof T> = { [P in keyof T as P extends K ? K : never]: T[P] }
+type Person = {
+  readonly name: string;
+  location: string;
+}
+
+type Equal<T,K> =
+(()=>A extends T ? true:false) extends (()=>B extends K ? true:false)
+? true
+: false
+
+type GetReadonlyKeys = keyof {
+[K in keyof T as Equal<Pick<T, K>, Readonly<Pick<T, K>>> extends true
+? K
+: never]: T[K]
+}
+
+const readonlyName: ExtractReadonly = {
+  name: 'mad',
+  location: 'we'
+}
+
+console.log(readonlyName)
