174494bd34ba8159630fe91bed238406f642ec6b
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-13 11:11:25 +0000

    e

--- .build/index.js
+++ .build/index.js
@@ -1,3 +1,95 @@
-const x = "mad";
-console.log(x);
+class Node {
+  data;
+  next;
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class LinkedList {
+  head;
+  size;
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.size++;
+  }
+  insertAt(data, index) {
+    if (index < 0 || index > this.size) {
+      return console.log("Please enter a valid index.");
+    } else {
+      const newNode = new Node(data);
+      let curr;
+      let prev;
+      curr = this.head;
+      if (index === 0) {
+        newNode.next = this.head;
+        this.head = newNode;
+      } else {
+        curr = this.head;
+        let it = 0;
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr.next;
+        }
+        newNode.next = curr;
+        prev.next = newNode;
+      }
+      this.size++;
+    }
+  }
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) {
+      return console.log("Please enter a valid index.");
+    } else {
+      let curr;
+      let prev;
+      let it = 0;
+      curr = this.head;
+      prev = curr;
+      if (index === 0) {
+        this.head = curr.next;
+      } else {
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr.next;
+        }
+        prev.next = curr.next;
+      }
+      this.size--;
+      return curr.data;
+    }
+  }
+  printList() {
+    let curr = this.head;
+    let str = "";
+    while (curr) {
+      str += curr.data + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+}
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.insertAt(0, 0);
+linkedList.printList();
+linkedList.removeFrom(1);
+linkedList.printList();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,22 +1,109 @@
-type Person = {
-  readonly name: string;
-  location: string;
+class Node {
+  data: number;
+  next: Node | null;
+
+  constructor(data: number) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-type Equal<T,K> =
-(()=>A extends T ? true:false) extends (()=>B extends K ? true:false)
-? true
-: false
+class LinkedList {
+  head: Node | null;
+  size: number;
 
-type GetReadonlyKeys = keyof {
-[K in keyof T as Equal<Pick<T, K>, Readonly<Pick<T, K>>> extends true
-? K
-: never]: T[K]
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(data: number): void {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.size++;
+  }
+
+  insertAt(data: number, index: number): void {
+    if (index < 0 || index > this.size) {
+      return console.log("Please enter a valid index.");
+    } else {
+      const newNode = new Node(data);
+      let curr: Node | null;
+      let prev: Node | null;
+
+      curr = this.head;
+
+      if (index === 0) {
+        newNode.next = this.head;
+        this.head = newNode;
+      } else {
+        curr = this.head;
+        let it = 0;
+
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr!.next;
+        }
+
+        newNode.next = curr;
+        prev!.next = newNode;
+      }
+      this.size++;
+    }
+  }
+
+  removeFrom(index: number): number | void {
+    if (index < 0 || index >= this.size) {
+      return console.log("Please enter a valid index.");
+    } else {
+      let curr: Node | null;
+      let prev: Node | null;
+      let it = 0;
+      curr = this.head;
+      prev = curr;
+
+      if (index === 0) {
+        this.head = curr!.next;
+      } else {
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr!.next;
+        }
+        prev!.next = curr!.next;
+      }
+      this.size--;
+
+      return curr!.data;
+    }
+  }
+
+  printList(): void {
+    let curr = this.head;
+    let str = "";
+    while (curr) {
+      str += curr.data + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
 }
 
-const readonlyName: ExtractReadonly = {
-  name: 'mad',
-  location: 'we'
-}
-
-console.log(readonlyName)
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.insertAt(0, 0);
+linkedList.printList();
+linkedList.removeFrom(1);
+linkedList.printList();
