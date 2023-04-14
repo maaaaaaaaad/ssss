a41a82c6c8aac465314e763a2ea1e50d7b0addf0
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-14 08:14:46 +0000

    wef

--- .build/index.js
+++ .build/index.js
@@ -1,95 +1,27 @@
-class Node {
-  data;
-  next;
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+class Member {
+  constructor(id) {
+    this.id = id;
+  }
+  get member() {
+    return this.id;
+  }
+  set member(id) {
+    this.id = id;
   }
 }
-class LinkedList {
-  head;
-  size;
-  constructor() {
-    this.head = null;
-    this.size = 0;
+class Room {
+  constructor(id) {
+    this.id = id;
   }
-  add(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-    this.size++;
+  get room() {
+    return this.id;
   }
-  insertAt(data, index) {
-    if (index < 0 || index > this.size) {
-      return console.log("Please enter a valid index.");
-    } else {
-      const newNode = new Node(data);
-      let curr;
-      let prev;
-      curr = this.head;
-      if (index === 0) {
-        newNode.next = this.head;
-        this.head = newNode;
-      } else {
-        curr = this.head;
-        let it = 0;
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr.next;
-        }
-        newNode.next = curr;
-        prev.next = newNode;
-      }
-      this.size++;
-    }
-  }
-  removeFrom(index) {
-    if (index < 0 || index >= this.size) {
-      return console.log("Please enter a valid index.");
-    } else {
-      let curr;
-      let prev;
-      let it = 0;
-      curr = this.head;
-      prev = curr;
-      if (index === 0) {
-        this.head = curr.next;
-      } else {
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr.next;
-        }
-        prev.next = curr.next;
-      }
-      this.size--;
-      return curr.data;
-    }
-  }
-  printList() {
-    let curr = this.head;
-    let str = "";
-    while (curr) {
-      str += curr.data + " ";
-      curr = curr.next;
-    }
-    console.log(str);
+  set room(id) {
+    this.id = id;
   }
 }
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.insertAt(0, 0);
-linkedList.printList();
-linkedList.removeFrom(1);
-linkedList.printList();
+const mad = new Member("OH");
+console.log(mad.member);
+mad.member = "mad";
+console.log(mad.member);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,109 +1,11 @@
-class Node {
-  data: number;
-  next: Node | null;
+class Room {
+  constructor(private id: string) { }
 
-  constructor(data: number) {
-    this.data = data;
-    this.next = null;
+  public get room() {
+    return this.id
+  }
+
+  public set room(id: string) {
+    this.id = id
   }
 }
-
-class LinkedList {
-  head: Node | null;
-  size: number;
-
-  constructor() {
-    this.head = null;
-    this.size = 0;
-  }
-
-  add(data: number): void {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-    this.size++;
-  }
-
-  insertAt(data: number, index: number): void {
-    if (index < 0 || index > this.size) {
-      return console.log("Please enter a valid index.");
-    } else {
-      const newNode = new Node(data);
-      let curr: Node | null;
-      let prev: Node | null;
-
-      curr = this.head;
-
-      if (index === 0) {
-        newNode.next = this.head;
-        this.head = newNode;
-      } else {
-        curr = this.head;
-        let it = 0;
-
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr!.next;
-        }
-
-        newNode.next = curr;
-        prev!.next = newNode;
-      }
-      this.size++;
-    }
-  }
-
-  removeFrom(index: number): number | void {
-    if (index < 0 || index >= this.size) {
-      return console.log("Please enter a valid index.");
-    } else {
-      let curr: Node | null;
-      let prev: Node | null;
-      let it = 0;
-      curr = this.head;
-      prev = curr;
-
-      if (index === 0) {
-        this.head = curr!.next;
-      } else {
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr!.next;
-        }
-        prev!.next = curr!.next;
-      }
-      this.size--;
-
-      return curr!.data;
-    }
-  }
-
-  printList(): void {
-    let curr = this.head;
-    let str = "";
-    while (curr) {
-      str += curr.data + " ";
-      curr = curr.next;
-    }
-    console.log(str);
-  }
-}
-
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.insertAt(0, 0);
-linkedList.printList();
-linkedList.removeFrom(1);
-linkedList.printList();
