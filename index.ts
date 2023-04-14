ca0c387b2e854c0ad2582f30bc3b7bbf0d850889
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-14 08:18:13 +0000

    e

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
 class Room {
   constructor(private id: string) { }
 
-  public get room() {
+  public get RoomId() {
     return this.id
   }
 
-  public set room(id: string) {
+  public set RoomId(id: string) {
     this.id = id
   }
 }
+
+class RoomRepository {
+  private rooms: Map<string, Room> = new Map<string, Room>()
+
+  public save(room: Room): void {
+    this.rooms.set(room.RoomId, room)
+  }
+
+  public find(roomId: string): Room | undefined {
+    return this.rooms.get(roomId)
+  }
+}
+
+class RoomService { }
