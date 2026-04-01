67a2c423512b4d81c6b56f004fc34a09c859d0f4
maaaaaaaaad <integral5137@gmail.com>
2026-04-01 21:14:06 +0900

    add: terraform outputs for all resources

---
+++ outputs.tf
@@ -0,0 +1,35 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "ec2_public_ip" {
+  value = aws_eip.backend.public_ip
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.backend.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
+
+output "rds_address" {
+  value = aws_db_instance.postgres.address
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "route53_nameservers" {
+  value = aws_route53_zone.main.name_servers
+}
+
+output "api_domain" {
+  value = aws_route53_record.api.fqdn
+}
+
+output "ssh_command" {
+  value = "ssh -i ~/.ssh/jellomark-ec2 ec2-user@${aws_eip.backend.public_ip}"
+}
