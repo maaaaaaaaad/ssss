e2a179f5f898e1d9be2a50a563f8c4768043bf83
maaaaaaaaad <integral5137@gmail.com>
2026-04-04 22:02:47 +0900

    add: s3 bucket outputs

--- outputs.tf
+++ outputs.tf
@@ -22,6 +22,14 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.backend.repository_url
 }
 
+output "s3_bucket_name" {
+  value = aws_s3_bucket.shop_images.id
+}
+
+output "s3_bucket_url" {
+  value = "https://${aws_s3_bucket.shop_images.bucket_regional_domain_name}"
+}
+
 output "ssh_command" {
   value = "ssh -i ~/.ssh/jellomark-ec2 ec2-user@${aws_eip.backend.public_ip}"
 }
